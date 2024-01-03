73bdb5b3e93ab7bc50fdb3c1485545dc8390af87
belo <pesadam15@gmail.com>
2024-01-03 16:39:44 +0100

    backend is back, nacitavanieie jsonu a nasledne jeho parsovanie a vytvaranie inputov

---
+++ backend/routes/GET/Dashboard.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+const pool = require('../../database/mysql_connect'); // Importujte váš MySQL pool
+
+router.get('/', (req, res) => {
+    const id = req.query.lang;
+    console.log(id);
+    pool.query('SELECT * FROM content WHERE id = ?', [id] , function(err, data){
+        if(err){
+            return res.status(500).send('Chyba servera');
+        }
+        
+        let htmlContent = '';
+        data.forEach(row => {
+            const contentObj = JSON.parse(row.value);
+
+            for (const [key, value] of Object.entries(contentObj)) {
+                htmlContent += `<label for="${key}">${key}</label>`;
+                htmlContent += `<input type="text" id="${key}" name="${key}" value="${value}"><br>`;
+            }
+        });
+
+        res.send(htmlContent)
+
+    });
+});
+
+module.exports = router;
